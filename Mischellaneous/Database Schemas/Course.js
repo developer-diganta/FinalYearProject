a003fa81785be082e766453372c2fed475b9e885
Diganta Kr Banik <digantabanik2000@gmail.com>
2022-06-02 09:46:19 +0530

    Issue#11:Added attributes for courses

---
+++ Mischellaneous/Database Schemas/Course.js
@@ -0,0 +1,10 @@
+course = {
+    name: String,
+    description: Array,
+    course_number: String,
+    course_type: String,
+    course_expected_duration: Number,
+    course_compilers: Array,
+    course_faculty: Array,
+    course_id: String,
+}
