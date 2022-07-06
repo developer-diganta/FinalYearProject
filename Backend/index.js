1f80215df24067b7d9c9a98857793ed088d0b335
Diganta Kr Banik <digantabanik2000@gmail.com>
2022-07-06 22:39:02 +0530

    Issue#23: Added MongoDB connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const mongoose = require("mongoose");
+const { url } = require("./Database/Connection");
 const getRoutes = require("./Routes/Get/get");
+const testConnection = require("./Database/connectionTest");
 // const postRoutes = require("./Routes/Post/post");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(cors());
+mongoose.connect(url, { useNewUrlParser: true });
+testConnection(url);
 
 app.use("/", getRoutes);
 
