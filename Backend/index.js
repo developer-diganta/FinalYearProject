bca3cfa21a26dd981bc302830b88448ce221b620
Diganta Kr Banik <digantabanik2000@gmail.com>
2022-07-07 19:29:05 +0530

    Issue#31: Added post routes for submission and languages

--- Backend/index.js
+++ Backend/index.js
@@ -5,13 +5,7 @@ const mongoose = require("mongoose");
 const { url } = require("./Database/Connection");
 const getRoutes = require("./Routes/Get/get");
 const testConnection = require("./Database/connectionTest");
-const admin = require("./Database/Schemas/Admin");
-const course = require("./Database/Schemas/Course");
-const teacher = require("./Database/Schemas/Teacher");
-const student = require("./Database/Schemas/Student");
-const university = require("./Database/Schemas/University");
-const question = require("./Database/Schemas/Question");
-// const postRoutes = require("./Routes/Post/post");
+const postRoutes = require("./Routes/Post/post");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
@@ -19,21 +13,11 @@ app.use(cors());
 mongoose.connect(url, { useNewUrlParser: true });
 testConnection(url);
 
-const adminSchema = new mongoose.Schema(admin);
-const courseSchema = new mongoose.Schema(course);
-const teacherSchema = new mongoose.Schema(teacher);
-const studentSchema = new mongoose.Schema(student);
-const universitySchema = new mongoose.Schema(university);
-const questionSchema = new mongoose.Schema(question);
-
-const Admin = mongoose.model("Admin", adminSchema);
-const Course = mongoose.model("Course", courseSchema);
-const Teacher = mongoose.model("Teacher", teacherSchema);
-const Student = mongoose.model("Student", studentSchema);
-const University = mongoose.model("University", universitySchema);
-const Question = mongoose.model("Question", questionSchema);
-
+// console.log(getRoutes)
 app.use("/", getRoutes);
+app.use("/", postRoutes);
+// app.use("/getLanguages", getLanguages);
+
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
