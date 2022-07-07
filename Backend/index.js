e6dd362c16b9182eaa2a694262363152af8cea6f
Diganta Kr Banik <digantabanik2000@gmail.com>
2022-07-07 11:50:03 +0530

    Issue#25:Added schemas to index.js

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,12 @@ const mongoose = require("mongoose");
 const { url } = require("./Database/Connection");
 const getRoutes = require("./Routes/Get/get");
 const testConnection = require("./Database/connectionTest");
+const admin = require("./Database/Schemas/Admin");
+const course = require("./Database/Schemas/Course");
+const teacher = require("./Database/Schemas/Teacher");
+const student = require("./Database/Schemas/Student");
+const university = require("./Database/Schemas/University");
+const question = require("./Database/Schemas/Question");
 // const postRoutes = require("./Routes/Post/post");
 
 app.use(express.json());
@@ -13,8 +19,21 @@ app.use(cors());
 mongoose.connect(url, { useNewUrlParser: true });
 testConnection(url);
 
-app.use("/", getRoutes);
+const adminSchema = new mongoose.Schema(admin);
+const courseSchema = new mongoose.Schema(course);
+const teacherSchema = new mongoose.Schema(teacher);
+const studentSchema = new mongoose.Schema(student);
+const universitySchema = new mongoose.Schema(university);
+const questionSchema = new mongoose.Schema(question);
 
+const Admin = mongoose.model("Admin", adminSchema);
+const Course = mongoose.model("Course", courseSchema);
+const Teacher = mongoose.model("Teacher", teacherSchema);
+const Student = mongoose.model("Student", studentSchema);
+const University = mongoose.model("University", universitySchema);
+const Question = mongoose.model("Question", questionSchema);
+
+app.use("/", getRoutes);
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
