9a10204e71c8f70e16546a0bc3c969b1a40d0e3c
Diganta Kr Banik <digantabanik2000@gmail.com>
2022-09-04 02:05:43 +0530

    Issue#41: Added rate-limit-config

---
+++ Backend/configs/cors-config.js
@@ -0,0 +1,14 @@
+//whitelist for cors
+const whitelist = ['*'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+module.exports = corsOptions;
