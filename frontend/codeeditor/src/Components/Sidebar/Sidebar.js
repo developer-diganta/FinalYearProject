48544affde91e57192558c8f61267a239cce15cf
surya-doc <suryadanabnk@gmail.com>
2022-09-18 17:21:27 +0530

    Updated design and added Redux

--- frontend/codeeditor/src/Components/Sidebar/Sidebar.js
+++ frontend/codeeditor/src/Components/Sidebar/Sidebar.js
@@ -1,49 +1,97 @@
 import React from 'react';
 import './Sidebar.css';
 
-function Sidebar() {
+function Sidebar({openSidebarVal}) {
+    console.log(openSidebarVal);
     const[sidebar, setSidebar] = React.useState('path')
   return (
-    <div className="sideb w-1/5 pt-4">
-        <div className="sec_1 pl-4 pb-2" style={{borderBottom: "1px solid #E0E0E0"}}>
-            <div className="flex items-center text-2xl gap-1">
-                <h1>L</h1>
-                <img className='w-10 h-10' style={{borderRadius: "50%", border: "1px solid #4D5559"}} src="/tan.png" alt="Coding Ninjas" />
-                <h1>GO</h1>
+    <div>
+        <div className="sideb w-1/5 pt-4 block md:hidden">
+            <div className="sec_1 pl-4 pb-2" style={{borderBottom: "1px solid #E0E0E0"}}>
+                <div className="flex items-center text-2xl gap-1">
+                    <h1>L</h1>
+                    <img className='w-10 h-10' style={{borderRadius: "50%", border: "1px solid #4D5559"}} src="/tan.png" alt="Coding Ninjas" />
+                    <h1>GO</h1>
+                </div>
+            </div>
+            <div className="sec_2">
+                <div className="profile flex flex-col items-center">
+                    <img className='w-24 h-24 shadow-xl mt-6' style={{borderRadius: "50%", border: "1px solid #b0b0b0"}} src="/profile.png" alt="" />
+                    <h1 className='text-xl font-bold text-text pt-4' >Tiasha Dhar</h1>
+                    <p className='text-sm font-bold text-text pt-2'>B.Tech IT</p>
+                    <div className='bg-divide w-4/5 my-8' style={{minWidth: "80px", minHeight: "1px"}}></div>
+                </div>
+            </div>
+            <div className="sec_3">
+                <div className={`elements my-4 flex gap-4 w-4/5 mx-auto py-2 pl-2 ${sidebar === 'dashboard' ? 'active' : 'not_active'}`}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#B0B0B0" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" />
+                    </svg>
+                    <h1 className='text-text font-bold'>Dashboard</h1>
+                </div>
+                <div className={`elements my-4 flex gap-4 w-4/5 mx-auto py-2 pl-2 ${sidebar === 'path' ? 'active' : 'not_active'}`}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#293462" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
+                    </svg>
+                    <h1 className="font-bold" style={{color: "#293462"}}>Path</h1>
+                </div>
+                <div className={`elements my-4 flex gap-4 w-4/5 mx-auto py-2 pl-2 ${sidebar === 'calender' ? 'active' : 'not_active'}`}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#B0B0B0" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+                    </svg>
+                    <h1 className='text-text font-bold'>Calender</h1>
+                </div>
+                <div className={`elements my-4 flex gap-4 w-4/5 mx-auto py-2 pl-2 ${sidebar === 'reminder' ? 'active' : 'not_active'}`}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#B0B0B0" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    <h1 className='text-text font-bold'>Reminder</h1>
+                </div>
             </div>
         </div>
-        <div className="sec_2">
-            <div className="profile flex flex-col items-center">
-                <img className='w-24 h-24 shadow-xl mt-6' style={{borderRadius: "50%", border: "1px solid #b0b0b0"}} src="/profile.png" alt="" />
-                <h1 className='text-xl font-bold text-text pt-4' >Tiasha Dhar</h1>
-                <p className='text-sm font-bold text-text pt-2'>B.Tech IT</p>
-                <div className='bg-divide w-4/5 my-8' style={{minWidth: "80px", minHeight: "1px"}}></div>
-            </div>
-        </div>
-        <div className="sec_3">
-            <div className={`elements my-4 flex gap-4 w-4/5 mx-auto py-2 pl-2 ${sidebar === 'dashboard' ? 'active' : 'not_active'}`}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#B0B0B0" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" />
-                </svg>
-                <h1 className='text-text font-bold'>Dashboard</h1>
-            </div>
-            <div className={`elements my-4 flex gap-4 w-4/5 mx-auto py-2 pl-2 ${sidebar === 'path' ? 'active' : 'not_active'}`}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#293462" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
-                </svg>
-                <h1 className="font-bold" style={{color: "#293462"}}>Path</h1>
-            </div>
-            <div className={`elements my-4 flex gap-4 w-4/5 mx-auto py-2 pl-2 ${sidebar === 'calender' ? 'active' : 'not_active'}`}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#B0B0B0" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
-                </svg>
-                <h1 className='text-text font-bold'>Calender</h1>
-            </div>
-            <div className={`elements my-4 flex gap-4 w-4/5 mx-auto py-2 pl-2 ${sidebar === 'reminder' ? 'active' : 'not_active'}`}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#B0B0B0" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-                <h1 className='text-text font-bold'>Reminder</h1>
+        <div className='hidden md:block'>
+            <div className={`sidebar_mobile w-1/4 sm:w-full ${openSidebarVal === true ? 'block' : 'hidden'}`}>
+                <div className="sec_1 pl-4 py-2" style={{borderBottom: "1px solid #E0E0E0"}}>
+                    <div className="flex items-center text-sm gap-1">
+                        <h1>L</h1>
+                        <img className='w-4 h-4' style={{borderRadius: "50%", border: "1px solid #4D5559"}} src="/tan.png" alt="Coding Ninjas" />
+                        <h1>GO</h1>
+                    </div>
+                </div>
+                <div className="sec_2">
+                    <div className="profile flex flex-col items-center">
+                        <img className='w-16 h-16 shadow-xl mt-6' style={{borderRadius: "50%", border: "1px solid #b0b0b0"}} src="/profile.png" alt="" />
+                        <h1 className='text-md font-bold text-text pt-4' >Tiasha Dhar</h1>
+                        <p className='text-xs font-bold text-text pt-2'>B.Tech IT</p>
+                        <div className='bg-divide w-full my-8' style={{minWidth: "80px", minHeight: "1px"}}></div>
+                    </div>
+                </div>
+                <div className="sec_3">
+                    <div className={`elements my-4 flex gap-4 w-full mx-auto py-2 pl-2 ${sidebar === 'dashboard' ? 'active' : 'not_active'}`}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#B0B0B0" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" />
+                        </svg>
+                        <h1 className='text-text font-bold'>Dashboard</h1>
+                    </div>
+                    <div className={`elements my-4 flex gap-4 w-full mx-auto py-2 pl-2 ${sidebar === 'path' ? 'active' : 'not_active'}`}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#293462" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
+                        </svg>
+                        <h1 className="font-bold" style={{color: "#293462"}}>Path</h1>
+                    </div>
+                    <div className={`elements my-4 flex gap-4 w-full mx-auto py-2 pl-2 ${sidebar === 'calender' ? 'active' : 'not_active'}`}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#B0B0B0" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+                        </svg>
+                        <h1 className='text-text font-bold'>Calender</h1>
+                    </div>
+                    <div className={`elements my-4 flex gap-4 w-full mx-auto py-2 pl-2 ${sidebar === 'reminder' ? 'active' : 'not_active'}`}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#B0B0B0" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        <h1 className='text-text font-bold'>Reminder</h1>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- frontend/codeeditor/src/Components/Student/Pages/Path/Path.js
+++ frontend/codeeditor/src/Components/Student/Pages/Path/Path.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import Sidebar from '../../../Sidebar/Sidebar';
 import './Path.css';
 
@@ -70,6 +71,9 @@ const questions = [
 ]
 
 function Path() {
+
+    const[openSidebar, setOpenSidebar] = useState(false);
+
   return (
     <div className="path flex w-full">
         {/* <div className='left 2xl:w-1/5'>
@@ -104,10 +108,18 @@ function Path() {
                 </div>
             </div>
         </div> */}
-        <Sidebar />
-        <div className='right 2xl:w-4/5'>
+        <Sidebar openSidebarVal={openSidebar} />
+        <div className="left 2xl:w-1/5 md:w-0"></div>
+        <div className='right 2xl:w-4/5 md:w-full'>
             <div className="right_header flex justify-between px-4 py-2">
-                <div className="name font-semibold text-2xl">Path</div>
+                <div className="name font-semibold text-2xl flex gap-4 items-center">
+                    <div className='md:block hidden' onClick={() => setOpenSidebar(!openSidebar)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="4" stroke="#FFF" class="w-8 h-8" style={{filter: 'drop-shadow(3px 3px 2px rgb(0 0 0 / 0.2))'}}>
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
+                        </svg>
+                    </div>
+                    Path
+                </div>
                 <div className="icons flex gap-2">
                     <div className="icon"></div>
                     <div className="icon"></div>
@@ -118,32 +130,41 @@ function Path() {
             <div className="path_name flex justify-center">
                 {val.path_name}
             </div>
-            <div className="sort_selection_area flex justify-end mr-10 text-sm">
+            <div className="sort_selection_area flex justify-end md:text-sm md:justify-center mr-10 text-sm">
                 <select id="countries" placeholder='Sory by difficulty' class="bg-white border text-lg border-gray-300 rounded-lg block w-48 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
                 {/* <option selected="">Sory by difficulty</option> */}
-                <option value="All">All</option>
-                <option value="US">Easy</option>
-                <option value="CA">Medium</option>
-                <option value="FR">Hard</option>
+                <option className='md:text-sm' value="All">All</option>
+                <option className='md:text-sm' value="US">Easy</option>
+                <option className='md:text-sm' value="CA">Medium</option>
+                <option className='md:text-sm' value="FR">Hard</option>
                 </select>
             </div>
             <div className="question_container w-4/5 mx-auto mt-8">
                 <div className="question_header flex justify-between w-full">
-                    <div className='w-3/5 text-lg font-bold'>Question</div>
-                    <div className='w-1/5 text-lg font-bold'>Difficulty</div>
-                    <div className='w-1/5 text-lg font-bold flex justify-center'>Status</div>
+                    <div className='w-3/5 sm:w-2/4 text-lg font-bold'>Question</div>
+                    <div className='w-1/5 sm:w-1/4 text-lg font-bold'>Difficulty</div>
+                    <div className='w-1/5 sm:w-1/4 text-lg font-bold flex justify-center'>Status</div>
                 </div>
                 <div className='bottom_divider min-h-1 min-w-full mb-8'></div>
                 <div className="questions">
-                    {questions.map((question) => (
+                    {questions.map((question, index) => (
                         <div className="indi_question flex justify-between w-full">
-                            <h4>{question.question}</h4>
+                            <h4>{index+1}</h4>
                             <p className='px-1'> . </p>
-                            <div className='w-3/5'>{question.description}</div>
+                            <div className='w-3/5 pr-4 question_description'>{question.description}</div>
                             <div className='w-1/5' style={{color: question.difficulty === 'Easy' ? '#30C8E8' : question.difficulty === 'Medium' ? '#FFB90F' : '#CB2A00'}}>{question.difficulty}</div>
                             <div className='w-1/5 flex justify-center'>{question.status === 'Solved' ? <div className='solve tooltip'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#048F09" class="w-4 h-4"><path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" /></svg><span class="tooltiptext text-xs font-thin">Solved</span></div> : <div className='not_solve tooltip'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#5E0101" class="w-3 h-3"><path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" /></svg><span class="tooltiptext text-xs font-thin">Not Solved</span></div>}</div>
                         </div>
                     ))}
+                </div>
+            </div>
+        </div>
+        <div className="screen_overlay md:block hidden">
+            <div style={{minHeight: '100vh', minWidth: "100vw", position: "fixed", backgroundColor: "rgba(0, 0, 0, 0.4)", zIndex: "888", top: "0", left: "0", overflow: "hidden", display: openSidebar === true ? 'block' : "none"}}>
+                <div className='flex justify-end z-999 fixed right-0' onClick={() => setOpenSidebar(!openSidebar)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
                 </div>
             </div>
         </div>
