e95b449a20583daae16a84c1e2a6e2de89417f54
surya-doc <suryadanabnk@gmail.com>
2022-09-30 21:23:08 +0530

    Updated changes

--- Backend/Routes/Get/get.js
+++ Backend/Routes/Get/get.js
@@ -5,7 +5,7 @@ const controls = require("../../controller/control");
 const router = express.Router();
 
 router.get('/', controls.home);
-router.get('/languages', authorise, controls.languages);
+router.get('/languages', controls.languages);
 router.get('/user/verified', controls.verified);
 router.get('/user/verify/:id/:uniqueString', controls.userVerify);
 const path = require('path');
