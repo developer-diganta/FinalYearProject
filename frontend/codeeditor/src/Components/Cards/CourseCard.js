fd5957b8adc89caa350f5ffd64e0a4f9cc2c12bd
surya-doc <suryadanabnk@gmail.com>
2022-10-29 23:08:02 +0530

    👌 IMPROVE: Updated university pages and routs

--- frontend/codeeditor/src/Components/Cards/CourseCard.js
+++ frontend/codeeditor/src/Components/Cards/CourseCard.js
@@ -1,26 +1,12 @@
 import React from 'react'
-import { FcFinePrint } from 'react-icons/fc'
-import { BsArrowRightShort } from 'react-icons/bs'
 
-function CourseCard() {
+function CourseCard({courseName}) {
   return (
-    <div>
-        <div className='teacher_card bg-white py-4 px-4 shadow-md rounded-md'>
-        <h1 className='text-center text-[#7F8C9D] text-2xl font-semibold' style={{borderBottom: "1px solid #D9D9D9"}}>Teachers</h1>
-        <div className='flex items-center justify-between py-8'>
-            <div>
-                total Courses <br />
-                <span className='font-bold text-3xl text-[#6D7276]'>60+</span>
-            </div>
-            <div className='border-2 border-[#9900ff] rounded-full p-2'>
-                <FcFinePrint className='text-4xl' />
-            </div>
-        </div>
-        <div className='flex items-center text-success font-bold text-xs cursor-pointer'>
-            See details
-            <BsArrowRightShort />
-        </div>
-    </div>
+    <div className='grid grid-cols-4 gap-14'>
+        <h1 className='bg-white border-2 border-[#6b00b3] flex justify-center items-center rounded-lg shadow-lg text-[#6b00b3] text-lg font-semibold transiti duration-150 hover:scale-105 hover:shadow-2xl' style={{maxWidth: "160px", minHeight: "160px", maxHeight: "160px"}}>{courseName}</h1>
+        <h1 className='bg-white border-2 border-[#6b00b3] flex justify-center items-center rounded-lg shadow-lg text-[#6b00b3] text-lg font-semibold transiti duration-150 hover:scale-105 hover:shadow-2xl' style={{maxWidth: "160px", minHeight: "160px", maxHeight: "160px"}}>{courseName}</h1>
+        <h1 className='bg-white border-2 border-[#6b00b3] flex justify-center items-center rounded-lg shadow-lg text-[#6b00b3] text-lg font-semibold transiti duration-150 hover:scale-105 hover:shadow-2xl' style={{maxWidth: "160px", minHeight: "160px", maxHeight: "160px"}}>{courseName}</h1>
+        <h1 className='bg-white border-2 border-[#6b00b3] flex justify-center items-center rounded-lg shadow-lg text-[#6b00b3] text-lg font-semibold transiti duration-150 hover:scale-105 hover:shadow-2xl' style={{maxWidth: "160px", minHeight: "160px", maxHeight: "160px"}}>{courseName}</h1>
     </div>
   )
 }
