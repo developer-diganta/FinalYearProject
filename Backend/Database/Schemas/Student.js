26179815724cde97caa72dd33a2c436b7688f353
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2023-02-15 00:49:22 +0530

    Update Student.js

--- Backend/Database/Schemas/Student.js
+++ Backend/Database/Schemas/Student.js
@@ -3,7 +3,7 @@ student = {
     age: Number,
     university: String,
     password: String,
-    status: String,
+    gender: String,
     courses: [{
         course_id: String,
         course_grade: String,
@@ -13,28 +13,11 @@ student = {
         progress: Number,
     }],
     address: {
-        street: String,
         city: String,
         state: String,
-        zip: String
+        country: String
     },
-    phone: String,
     email: String,
-    date_of_birth: Date,
-    stream: String,
-    admission_date: Date,
-    graduation_date: Date,
-    graduation_status: String,
-    graduation_status_date: Date,
-    progress:{
-        progress_id: String,
-        progress_name: String,
-        progress_grade: Number,
-        completed: Boolean,
-        completed_date: Date,
-        start_date: Date,
-    },
-    submissions:Array
 }
 
 module.exports = student;
