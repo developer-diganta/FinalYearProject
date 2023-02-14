629488e301d20999829ed10818ac5108e42ab91c
surya-doc <suryadanabnk@gmail.com>
2023-02-14 19:22:03 +0530

    Revert "👌 IMPROVE: Added updated pages"
    This reverts commit c4c5ada0368d8cd22d790bc1292225dcd4219eb2.

--- frontend/src/Components/Teacher/Courses/DifferentOptions/Question.js
+++ frontend/src/Components/Teacher/Courses/DifferentOptions/Question.js
@@ -1,47 +1,32 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import { backend_url } from '../../../../BackendRoutes';
 import '../Courses.css'
 
 function Question({coursestate, courseDetail}) {
   const navigate = useNavigate();
-  const[allQuestions, setAllQuestions] = useState([]);
-//   const demo__questions = [
-//   {
-//     title: "String",
-//     question: "String",
-//     question_id : "093473047036",
-//     course_id : "9043734706",
-//     difficulty: "Easy",
-//   },
-//   {
-//     title: "String",
-//     question: "String",
-//     question_id : "093473047036",
-//     course_id : "9043734706",
-//     difficulty: "Medium",
-//   },
-//   {
-//     title: "String",
-//     question: "String",
-//     question_id : "093473047036",
-//     course_id : "9043734706",
-//     difficulty: "Hard",
-//   }
-// ]
-
-  async function getQuestions(){
-    const course__id = courseDetail._id;
-    console.log("33.", course__id);
-    const questions = await axios.post(backend_url + '/teacher/course/questions', {courseId: course__id});
-    console.log("questions.", questions);
-    setAllQuestions(questions.data);
+  const demo__questions = [
+  {
+    title: "String",
+    question: "String",
+    question_id : "093473047036",
+    course_id : "9043734706",
+    difficulty: "Easy",
+  },
+  {
+    title: "String",
+    question: "String",
+    question_id : "093473047036",
+    course_id : "9043734706",
+    difficulty: "Medium",
+  },
+  {
+    title: "String",
+    question: "String",
+    question_id : "093473047036",
+    course_id : "9043734706",
+    difficulty: "Hard",
   }
-
-  useEffect(() => {
-    getQuestions();
-  }, [])
+]
 
   return (
     <div className='questions'>
@@ -49,28 +34,26 @@ function Question({coursestate, courseDetail}) {
         <div className='create__course__button' onClick={() => navigate('/teacher/createquestion', {state: courseDetail})}>Create Question +</div>
       </div>
       {
-        allQuestions ? allQuestions.map((question, index) => {
+        demo__questions.map((question) => {
           return (
-            <div className='question bg-white my-6 py-6 px-4 flex justify-between items-end rounded-sm shadow-lg cursor-pointer hover:text-[#C11FF5]' onClick={() => navigate('/teacher/question/questiondetail', {state: {question, courseDetail: courseDetail, questionNo: index+1}})}>
+            <div className='question bg-white my-6 py-6 px-4 flex justify-between items-end rounded-sm shadow-lg'>
               <div className=''>
                 <div className='question__title'>
-                  <h3 className='text-xl font-semibold pb-1' style={{fontFamily: "Whitney SSm A,Whitney SSm B,Avenir,Segoe UI,Ubuntu,Helvetica Neue,Helvetica,Arial,sans-serif"}}>{index+1}. {question.title}</h3>
+                  <h3 className='text-xl font-bold'>{question.title}</h3>
                 </div>
                 <div className='question__difficulty'>
                   <p className={`text-xs ${question.difficulty === 'Easy' ? 'text-[#97D01E]' : question.difficulty === 'Medium' ? 'text-[#FEC831]' : 'text-[#FB4A3F]'} font-semibold`}>{question.difficulty}</p>
                 </div>
-                {/* <div className='question__question pt-4'>
-                  <p className='text-base text-[#000000]'>{question.question}</p>
-                </div> */}
+                <div className='question__question pt-4'>
+                  <p className='text-base'>{question.question}</p>
+                </div>
               </div>
-              {/* <div>
+              <div>
                 <div className='solve__question px-4 py-2 rounded-sm hover:scale-105 cursor-pointer hover:shadow-lg'>Solve Question</div>
-              </div> */}
+              </div>
             </div>
           )
         })
-        :
-        console.log("")
       }
     </div>
   )
